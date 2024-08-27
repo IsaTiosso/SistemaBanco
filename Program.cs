1bf231f44456d15c90482acd8bbe28e75b5f71f6
IsaTiosso <165043836+IsaTiosso@users.noreply.github.com>
2024-08-27 16:40:26 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿public class Conta
+{//Conta 
+    // declaração das variáveis com seus tipos de dado e acesso privado 
+    private int agenda;
+    private int numero;
+    private string nomeCliente;
+    private decimal valorDisponível; 
+
+
+}//Conta
