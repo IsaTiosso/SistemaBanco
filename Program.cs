eb374dc91f85ae0fe4cec8c4401164dfd8190039
IsaTiosso <165043836+IsaTiosso@users.noreply.github.com>
2024-08-28 16:23:14 -0300

    28/08 - get e set

--- Program.cs
+++ Program.cs
@@ -1,10 +1,45 @@
 ﻿public class Conta
 {//Conta 
     // declaração das variáveis com seus tipos de dado e acesso privado 
-    private int agenda;
+    private int agencia;
     private int numero;
     private string nomeCliente;
-    private decimal valorDisponível; 
+    private decimal valorDisponivel;
 
+    public int Agencia
+    {
+        get { return agencia; }
+        set
+        {
+            if (value.ToString().Length == 4)
+            {
+                agencia = value;
+            }
+            else
+            {
+                throw new ArgumentException("O código da agência deve ter 4 dígitos");
+
+            }
+        }
+    }
+
+    public int Numero
+    {
+        get { return numero; }
+        set
+        {
+            if (value.ToString().Lenght == 8)
+            {
+                numero = value;
+            }
+            else
+            {
+                throw new ArgumentException("O número da conta deve ter 8 dígitos");
+            }
+        }
+
+    }
+
+    public string NomeCliente { get => nomeCliente; set => nomeCliente = value; }
 
 }//Conta
